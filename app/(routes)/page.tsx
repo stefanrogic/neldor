9022652e2efbdcb976701d71862a2004d6aae553
stefanrogic <Stefanrogic@protonmail.com>
2025-04-02 17:42:26 +0200

    added routes, data and fixed errors

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,6 +2,9 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { files } from "@/app/data/data";
+
+import Link from "next/link";
 
 export default function Page() {
   return (
@@ -19,18 +22,22 @@ export default function Page() {
                 </BreadcrumbItem>
                 <BreadcrumbSeparator className="hidden md:block" />
                 <BreadcrumbItem>
-                  <BreadcrumbPage>Your Panel</BreadcrumbPage>
+                  <BreadcrumbPage>Your Files</BreadcrumbPage>
                 </BreadcrumbItem>
               </BreadcrumbList>
             </Breadcrumb>
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-2">
-            <div className="aspect-video rounded-xl bg-muted/50"></div>
-            <div className="aspect-video rounded-xl bg-muted/50"></div>
+          <div className="grid grid-flow-col grid-cols-10 gap-3">
+            {files.map((f, i) => (
+              <Link key={i} href={"/editor/" + f.slug} className="flex flex-col gap-y-2">
+                <div className="aspect-square bg-gray-400"></div>
+                <span>{f.name}</span>
+              </Link>
+            ))}
+            <button className="flex flex-col justify-center items-center aspect-square border border-gray-400 hover:bg-gray-400">New File</button>
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min"></div>
         </div>
       </SidebarInset>
     </SidebarProvider>
--- components/team-switcher.tsx
+++ components/team-switcher.tsx
@@ -23,7 +23,7 @@ export function TeamSwitcher({
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <h1 className="font-bold text-2xl">Neldor</h1>
+        <h1 className="font-bold text-2xl p-2">Neldor</h1>
         {/* <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <SidebarMenuButton
