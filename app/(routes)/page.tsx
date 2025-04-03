94c45794695f09eafdcd6f725744b3c1d7370d17
stefanrogic <Stefanrogic@protonmail.com>
2025-04-03 11:50:31 +0200

    new document button functionality and fixed grid

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="grid grid-flow-col grid-cols-12 gap-3">
+    <div className="grid grid-flow-row grid-cols-2 md:grid-cols-4 xl:grid-cols-8 gap-3">
       {files.map((f, i) => (
         <Link key={i} href={"/editor/" + f.slug} className="flex flex-col gap-y-2">
           <div className="flex justify-center items-center aspect-square bg-gray-200">
@@ -15,9 +15,9 @@ export default function Page() {
           <span className="text-sm">{f.name}</span>
         </Link>
       ))}
-      <button className="flex flex-col justify-center items-center aspect-square border border-gray-200 hover:bg-gray-200">
+      <Link href="/editor/new" className="flex flex-col justify-center items-center aspect-square border border-gray-200 hover:bg-gray-200">
         <IoAddCircleOutline size={50} color="gray" />
-      </button>
+      </Link>
     </div>
   );
 }
